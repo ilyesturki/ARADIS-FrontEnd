85058b838223d51bb74eae106dbdbe31bdcb9e6f
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-03-11 16:44:50 +0000

    update the project

--- components/Dashboard/Panel/Fps/FpssPanelDashboard.tsx
+++ components/Dashboard/Panel/Fps/FpssPanelDashboard.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import CustomPieChart from "../Chart/CustomPieChart";
+import CustomAreaChart from "../Chart/CustomAreaChart";
+import CustomBarChart from "../Chart/CustomBarChart";
+import CustomPieChartLabel from "../Chart/CustomPieChartLabel";
 
 const FpssPanelDashboard = () => {
   return (
-    <div className="grid grid-cols-3 grid-rows-2 gap-6">
-      <CustomPieChart />
-      <CustomPieChart />
-      <CustomPieChart />
+    <div className="grid grid-cols-3 grid-rows-3 gap-6">
+      <div className="col-span-3">
+        <CustomBarChart />
+      </div>
+      <div className="col-span-2 flex items-center">
+        <CustomAreaChart className="flex-1" />
+      </div>
+      <CustomPieChartLabel />
       <CustomPieChart />
       <CustomPieChart />
       <CustomPieChart />
