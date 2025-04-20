3a2a94067e4f3e5e69bb1e4d6c26f02cd108c071
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-04-20 23:22:04 +0000

    update the project

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "react-hot-toast";
 import NextSessionProvider from "@/components/Provider/NextSessionProvider";
 import StoreProvider from "@/redux/StoreProvider";
 
-import { NextIntlClientProvider, Locale, hasLocale } from "next-intl";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 
