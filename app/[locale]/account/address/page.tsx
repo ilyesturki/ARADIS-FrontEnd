448313fceeb0ba71186b59ee00b960875191c103
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-04-20 21:55:59 +0000

    update the project

--- app/[locale]/account/address/page.tsx
+++
@@ -1,7 +0,0 @@
-// import Address from "@/components/Account/Address/Address";
-
-const page = () => {
-  //   return <Address />;
-};
-
-export default page;
--- app/[locale]/account/page.tsx
+++
@@ -1,7 +0,0 @@
-import General from "@/components/Account/General/General";
-
-const page = async () => {
-  return <General />;
-};
-
-export default page;
