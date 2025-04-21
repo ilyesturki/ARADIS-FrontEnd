935a570becfd35e5591b960bddb2e9ff9cbc7fe3
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-04-21 00:24:26 +0000

    update the project

--- app/[locale]/dashboard/users/edit-user/[id]/page.tsx
+++ app/[locale]/dashboard/users/edit-user/[id]/page.tsx
@@ -1,7 +1,8 @@
 import EditUser from "@/components/Dashboard/User/EditUser/EditUser";
 
-const Page = ({ params }: { params: { id: string } }) => {
-  return <EditUser id={params.id} />;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  return <EditUser id={id} />;
 };
 
 export default Page;
