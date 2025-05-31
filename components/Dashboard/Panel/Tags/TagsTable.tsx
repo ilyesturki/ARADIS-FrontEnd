13cbbc7069c94fa9886a3d3cc86e92d9d4ec243f
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-05-31 18:31:06 +0000

    update the project

--- components/Dashboard/Panel/Tags/TagsTable.tsx
+++ components/Dashboard/Panel/Tags/TagsTable.tsx
@@ -4,18 +4,21 @@ import { columns } from "@/components/Common/DataTable/Columns/TagsColumn";
 import { fetchTags } from "@/utils/Api/tagApi";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
+import { useAppSelector } from "@/redux/hooks";
 
 const TagsTable = () => {
   const t = useTranslations("TagsPanelPage");
   const [tags, setTags] = useState([]);
 
+  const machine = useAppSelector((state) => state.tags.machine);
+
   useEffect(() => {
     const loadTags = async () => {
-      const response = await fetchTags();
+      const response = await fetchTags(`?machine=${machine}`);
       setTags(response.data);
     };
     loadTags();
-  }, []);
+  }, [machine]);
 
   return <DataTable columns={columns(t)} data={tags} />;
 };
